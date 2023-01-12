45ce5da99c44fb16d932a974bf74bda80cc9ab82
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2023-01-12 18:07:11 +0530

    Create post modal

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -6,33 +6,95 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Travelmania | 	Home</title>
 
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/home.css">
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+	<script>
+		let input = document.getElementById("customFile");
+		let preview = document.querySelector(".img-preview");
+		input.addEventListener("change", function () {
+			let file = this.files[0];
+			let reader = new FileReader();
+			reader.addEventListener("load", function () {
+				preview.src = reader.result;
+			}, false);
+			if (file) {
+				reader.readAsDataURL(file);
+			}
+		});
+	</script>
 </head>
 <body>
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="col-8">
-				<div class="row align-items-center justify-content-center">
-					<div class="col-6">
-						<h1 class="text-left">Travelmania</h1>
-					</div>
-					<div class="col-6 text-right">
-						<form action="<?php echo base_url(); ?>index.php/auth/logout">
-							<button class="btn btn-secondary mx-auto">Logout</button>
-						</form>
-					</div>
-				</div>
 				<div class="row">
 					<div class="col-12">
+						<div class="row">
+							<div class="col align-self-start">
+								<h2 class="text-left">Welcome <?php echo $this->session->userdata('user')['username']; ?>! 👋</h2>
+								<span>Discover and Explore new places near you!</span>
+							</div>
+							<div class="col align-self-end">
+								<button class="btn btn-lg btn-dark rounded float-right" data-toggle="modal" data-target="#exampleModal">
+									Create Post
+								</button>
+
+								<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+									<div class="modal-dialog-scrollable modal-dialog-centered rounded" role="document">
+										<div class="modal-content bg-black text-white">
+											<div class="modal-header">
+												<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+												<button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+													<span aria-hidden="true">&times;</span>
+												</button>
+											</div>
+											<div class="modal-body">
+												<div class="row">
+													<div class="col-6">
+														<div class="custom-file">
+															<input type="file" class="custom-file-input" id="customFile" accept="image/*">
+															<label class="custom-file-label" for="customFile">
+															</label>
+
+															<img src="<?php echo base_url(); ?>assets/images/register_background.jpg" class="img-thumbnail" alt="Placeholder image">
+														</div>
+
+													</div>
+													<div class="col-6">
+														<textarea class="form-control rounded-pill border border-white bg-black text-white"></textarea>
+													</div>
+												</div>
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+												<button type="button" class="btn btn-primary">Save changes</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
 						<br>
-						<h2 class="text-left">Welcome <?php echo $this->session->userdata('user')['username']; ?>! 👋</h2>
-						<span>Discover and Explore new places near you!</span>
-						<button class="btn btn-lg btn-dark rounded float-right">Create Post</button>
+						<div class="row">
+							<h2>Popular Destinations</h2>
+						</div>
+
+						<br>
+						<div class="row">
+							<h2>Recently Added</h2>
+						</div>
+
+						<br>
+						<div class="row">
+							<h2>Places Near Me</h2>
+						</div>
 					</div>
+
 				</div>
 			</div>
 			<div class="col-4 bg-danger rounded h-100">
